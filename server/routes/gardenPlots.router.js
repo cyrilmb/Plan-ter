9f92cd1d85377b866cc28011ebb2a51955f703ec
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-06-27 16:44:34 -0500

    router to GET user garden plots set up

--- server/routes/gardenPlots.router.js
+++ server/routes/gardenPlots.router.js
@@ -5,4 +5,22 @@ const {
   rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
 
+//Get user's garden plot info
+router.get('/:userId', rejectUnauthenticated, async (req, res) => {
+  try {
+    const queryText = `SELECT * FROM "plot_library"
+      WHERE "user_id" = $1
+      `;
+    const gardenPlotsResponse = await pool.query(queryText, [
+      req.params.userId,
+    ]);
+    const plots = gardenPlotsResponse.rows;
+
+    res.send(plots);
+  } catch (error) {
+    res.sendStatus(500);
+    console.log('Error getting user garden plots:', error);
+  }
+});
+
 module.exports = router;

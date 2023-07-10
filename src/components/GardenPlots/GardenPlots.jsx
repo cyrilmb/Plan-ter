a93ff77a02614e4095a281ded57fbef3914cb6f5
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-07-10 15:49:48 -0500

    canvas now rendering!

--- src/components/GardenPlots/GardenPlots.jsx
+++ src/components/GardenPlots/GardenPlots.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
+import Canvas from "../CanvasComponent";
+
 function GardenPlots() {
     const dispatch = useDispatch();
     const user = useSelector(store => store.user);
@@ -8,27 +10,16 @@ function GardenPlots() {
 
     const userId = user.id;
 
-    function draw() {
-        const canvas = document.getElementById("canvas");
-        const ctx = canvas.getContext("2d");
+    useEffect(() => {
+        dispatch({
+            type: 'FETCH_GARDEN_PLOTS',
+            payload: userId
+        });
+    }, []);
 
-        ctx.fillStyle = "rgb(200, 0, 0)";
-        ctx.fillRect(10, 10, 50, 50);
-
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect(30, 30, 50, 50);
-    }
-}
-draw();
-
-useEffect(() => {
-    dispatch({
-        type: 'FETCH_GARDEN_PLOTS',
-        payload: userId
-    });
-}, []);
-
-return;
+    return (
+        <Canvas />
+    );
 }
 
 export default GardenPlots;

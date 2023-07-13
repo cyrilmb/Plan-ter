67469de235b0c44c924715f7768380437ecede82
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-07-12 22:34:01 -0500

    played around with tailwind a bit. trying to figure out dynamic canvas rendering

--- src/components/GardenPlots/GardenPlots.jsx
+++ src/components/GardenPlots/GardenPlots.jsx
@@ -17,30 +17,33 @@ function GardenPlots() {
         });
     }, []);
 
+    function submitYardDimensions() {
+
+    }
+
     return (
         <div>
             <Canvas />
             <div class="container mx-auto p-4">
                 <form class="bg-pink-500 shadow-md rounded px-8 pt-6 pb-8 mb-4">
                     <div class="mb-4">
-                        <label class="block text-white text-sm font-bold mb-2" for="name">
-                            Name
+                        <label class="block text-white text-sm font-bold mb-2" for="height">
+                            Height
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Enter your name"></input>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="height-input" type="int" placeholder="Enter the height in meters"></input>
                     </div>
                     <div class="mb-4">
-                        <label class="block text-white text-sm font-bold mb-2" for="email">
-                            Email
+                        <label class="block text-white text-sm font-bold mb-2" for="width">
+                            Width
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Enter your email"></input>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="width-input" type="int" placeholder="Enter the width in meters"></input>
                     </div>
                     <div class="flex items-center justify-between">
-                        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        <button
+                            onClick={submitYardDimensions()}
+                            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                             Submit
                         </button>
-                        <a class="inline-block align-baseline font-bold text-sm text-yellow-400 hover:text-yellow-600" href="#">
-                            Forgot Password?
-                        </a>
                     </div>
                 </form>
             </div>
